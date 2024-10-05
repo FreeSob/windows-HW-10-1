508250858be736a263502d248e740f311df683f2
FREE <84310382+FreeSob@users.noreply.github.com>
2024-10-05 16:22:35 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <QApplication>
+#include <QTabWidget>
+#include <QLabel>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QFileDialog>
+#include <QColorDialog>
+#include <QFontDialog>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QTabWidget *tabWidget = new QTabWidget;
+    QWidget *leaderTab = new QWidget;
+    QLabel *leaderLabel = new QLabel("隊長\n組員1\n組員2\n組員3");
+    leaderLabel->setStyleSheet("color: blue;");
+    QVBoxLayout *leaderLayout = new QVBoxLayout;
+    leaderLayout->addWidget(leaderLabel);
+    leaderTab->setLayout(leaderLayout);
+
+   
+    tabWidget->addTab(leaderTab, "隊長");
+    tabWidget->addTab(member1Tab, "組員1");
+    tabWidget->addTab(member2Tab, "組員2");
+    tabWidget->addTab(member3Tab, "組員3");
+
+    tabWidget->setWindowTitle("example");
+    tabWidget->resize(400, 300);
+    tabWidget->show();
+
+    return app.exec();
+}
